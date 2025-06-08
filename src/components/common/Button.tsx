773d0a2777c0e3ed128d51eb25cc246e2d5184dc
Suji Hwang <soojiya2003@gmail.com>
2025-06-08 23:49:21 +0900

    ♻️Refactor: twMerge 사용 -> 불필요한 코드 삭제

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,52 +1,19 @@
-type FontWeightOption =
-  | "light"
-  | "regular"
-  | "medium"
-  | "semiBold"
-  | "bold"
-  | "extraBold";
-const weightMap: Record<FontWeightOption, number> = {
-  light: 300,
-  regular: 400,
-  medium: 500,
-  semiBold: 600,
-  bold: 700,
-  extraBold: 800,
-};
+import { twMerge } from "tailwind-merge";
 export default function Button({
-  fontSize,
-  fontWeight = "semiBold",
-  btnPX,
-  btnPY,
-  backgroundColor,
   children,
+  className,
   onClick,
 }: {
-  fontSize: number;
-  fontWeight?: FontWeightOption;
-  btnPX: number;
-  btnPY: number;
-  backgroundColor: string;
+  className: string;
   children: React.ReactNode;
   onClick?: () => void;
 }) {
-  const numericWeight = weightMap[fontWeight];
-
+  const buttonBasicStyle =
+    "border-2 border-[var(--black)] shadow-[0px_5px_0px_var(--black)] rounded-[6px] text-center cursor-pointer font-semibold text-[18px] text-center align-middle w-[100px] h-[39px]";
   return (
     <>
       <button
-        style={{
-          fontSize: `${fontSize}px`,
-          fontWeight: `${numericWeight}`,
-          //py: padding y값, px: padding x값
-          padding: `${btnPY}px ${btnPX}px `,
-          backgroundColor: `${backgroundColor}`,
-          border: "2px solid var(--black)",
-          boxShadow: "0px 5px 0px var(--black)",
-          borderRadius: "6px",
-          textAlign: "center",
-          cursor: "pointer",
-        }}
+        className={twMerge(buttonBasicStyle, className)}
         onClick={onClick}>
         {children}
       </button>
