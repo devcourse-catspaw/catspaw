72953030336add99a25497af1ea80e9b4c588811
llyybbb <yb9766@naver.com>
2025-06-09 02:06:34 +0900

    🎨 Design: PostList 컴포넌트 제작

---
+++ src/components/common/PostList.tsx
@@ -0,0 +1,21 @@
+type PostListProps = {
+  postTitle: string
+  comments: number
+  date: string
+}
+
+export default function PostList({ postTitle, comments, date }: PostListProps) {
+  return (
+    <>
+      <div className="w-[688px] h-[55px] px-[20px] flex justify-between items-center">
+        <div className="flex justify-between items-center gap-[13px]">
+          <span className="font-semibold text-[15px] truncate max-w-[300px] overflow-hidden whitespace-nowrap">
+            {postTitle}
+          </span>
+          <span className="text-[12px]">[{comments}]</span>
+        </div>
+        <div className="text-[12px]">{date}</div>
+      </div>
+    </>
+  )
+}
