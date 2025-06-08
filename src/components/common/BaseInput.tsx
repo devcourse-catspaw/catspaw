2c7da2fd6843089e5b59447dde56bf9f9567fcdf
Jungyu Park <parkjungyu09@gmail.com>
2025-06-08 17:29:08 +0900

    🎨 Design: BaseInput 공통 컴포넌트 생성

--- src/components/common/BaseInput.tsx
+++ src/components/common/BaseInput.tsx
@@ -1,7 +1,20 @@
-export default function BaseInput() {
+import type { InputHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+export default function BaseInput({ className, ...props }: BaseInputProps) {
+  const baseInputStyle =
+    "border-2 border-[color:var(--black)] px-4 py-3 rounded-[3px] w-full text-lg";
   return (
     <>
-      <input type="text" className="border-2" placeholder="제시어 입력"/>
+      <input
+        type="text"
+        className={twMerge(baseInputStyle, className)}
+        placeholder="제시어 입력"
+        {...props}
+      />
     </>
   );
 }
