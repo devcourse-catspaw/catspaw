efd47a63e04519dfd79c75bfb4bbb5f20fe02a82
yubin121 <yubin045@naver.com>
2025-06-08 16:13:00 +0900

    🎨 Design: 공통 GameRoom 컴포넌트 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import GameRoom from './components/common/GameRoom';
+
 export default function App() {
   return (
     <>
       <h1>App Component</h1>
+      <GameRoom />
     </>
   );
 }
---
+++ src/components/common/GameRoom.tsx
@@ -0,0 +1,7 @@
+export default function GameRoom() {
+  return (
+    <>
+      <h1>GameRoom Component</h1>
+    </>
+  );
+}
