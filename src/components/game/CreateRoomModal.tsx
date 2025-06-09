9512f2602fc0bdf005ccdcccc4df494c593fdd44
yubin121 <yubin045@naver.com>
2025-06-09 21:33:48 +0900

    🎨 Design: 게임방 만들기 모달 구현

---
+++ src/components/game/CreateRoomModal.tsx
@@ -0,0 +1,56 @@
+import Button from '../common/Button';
+import LabeledInput from '../common/LabeledInput';
+import close from '../../assets/images/icon_close.svg';
+
+export default function CreateRoomModal({
+  closeCreateRoomModalHandler,
+}: {
+  closeCreateRoomModalHandler: () => void;
+}) {
+  return (
+    <>
+      <div
+        className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] flex justify-center items-center z-[1000]"
+        onClick={closeCreateRoomModalHandler}
+      >
+        <div
+          className="rounded-[6px] w-[519px] pb-11 flex flex-col gap-3 bg-[var(--white)] border-2 border-[var(--black)]"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="flex justify-end pt-5 pr-6">
+            <img
+              src={close}
+              alt="닫기"
+              onClick={closeCreateRoomModalHandler}
+              className="cursor-pointer"
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-[22px]">
+            <div className="font-semibold text-[24px]">게임방 생성</div>
+            <div className="flex flex-col gap-3">
+              <LabeledInput
+                title="게임방 이름"
+                invalidMessage="한 글자 이상 입력해주세요."
+                isInvalid={false}
+                placeholder="이름 입력"
+                className="w-[339px] h-[50px] pr-[50px]"
+              />
+              <LabeledInput
+                type="password"
+                title="게임방 비밀번호"
+                invalidMessage="비밀번호가 일치하지 않습니다."
+                isInvalid={false}
+                placeholder="비밀번호 입력"
+                className="w-[339px] h-[50px] pr-[50px]"
+              />
+            </div>
+            <div className="flex gap-8">
+              <Button>생성</Button>
+              <Button onClick={closeCreateRoomModalHandler}>취소</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
