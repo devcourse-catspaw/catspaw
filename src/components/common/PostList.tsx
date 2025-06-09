2081a10f64e600fbb2b027eb4a93175a4c2af1ec
llyybbb <yb9766@naver.com>
2025-06-09 09:46:31 +0900

    🎨 Design: gap 12px로 수정

--- src/components/common/PostList.tsx
+++ src/components/common/PostList.tsx
@@ -8,7 +8,7 @@ export default function PostList({ postTitle, comments, date }: PostListProps) {
   return (
     <>
       <div className="w-[688px] h-[55px] px-[20px] flex justify-between items-center">
-        <div className="flex justify-between items-center gap-[13px]">
+        <div className="flex justify-between items-center gap-[12px]">
           <span className="font-semibold text-[15px] truncate max-w-[300px] overflow-hidden whitespace-nowrap">
             {postTitle}
           </span>
