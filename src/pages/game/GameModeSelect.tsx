332d8e6cd9809d3ef50a35c20e15869116555eb3
Jungyu Park <parkjungyu09@gmail.com>
2025-06-09 23:23:47 +0900

    ✨ Feature: 싱글모드 클릭 시 숫자 카운트 후 페이지 이동 구현

--- src/pages/game/GameModeSelect.tsx
+++ src/pages/game/GameModeSelect.tsx
@@ -1,24 +1,52 @@
-import NavWithExit from "../../components/common/NavWithExit";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import GameModeSelectCard from "../../components/game/game_mode/GameModeSelectCard";
+import NavWithExit from "../../components/common/NavWithExit";
 import pawPencil from "../../assets/images/paw_pencil_big.svg";
 import doodle from "../../assets/images/doodle_loading.svg";
-import { useNavigate } from "react-router";
 
 export default function GameModeSelect() {
   const navigate = useNavigate();
+  const [count, setCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (count === null) return;
+    if (count === 0) {
+      navigate("/game/single");
+      return;
+    }
+    const timer = setTimeout(() => {
+      setCount((prev) => (prev ?? 0) - 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [count, navigate]);
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px] relative">
       <NavWithExit />
       <div className="flex flex-col justify-center items-center gap-10 absolute inset-0">
-        <p className="font-extrabold text-[39px]">게임 모드를 골라보세요!</p>
-        <div className="flex gap-[81px]">
-          <div onClick={() => navigate("/game/single")} className="z-50">
-            <GameModeSelectCard mode="single" />
+        {count === null ? (
+          <>
+            <p className="font-extrabold text-[39px]">
+              게임 모드를 골라보세요!
+            </p>
+            <div className="flex gap-[81px]">
+              <div
+                onClick={() => setCount(3)} 
+                className="z-50 cursor-pointer"
+              >
+                <GameModeSelectCard mode="single" />
+              </div>
+              <div onClick={() => navigate("/game/multi")} className="z-50">
+                <GameModeSelectCard mode="multi" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="text-[134px] font-gloria font-extrabold text-[color:var(--black)] animate-pulse z-50">
+            {count}
           </div>
-          <div onClick={() => navigate("/game/multi")} className="z-50">
-            <GameModeSelectCard mode="multi" />
-          </div>
-        </div>
+        )}
       </div>
       <img
         src={pawPencil}
