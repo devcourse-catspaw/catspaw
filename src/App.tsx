dd4f0a47ae9c59ac6b8ca4958dd910d815fd1dd2
Jungyu Park <parkjungyu09@gmail.com>
2025-06-09 10:41:52 +0900

    ♻️ Refactor: App.tsx 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import ChatMessage from "./components/common/ChatMessage";
-
 export default function App() {
   return (
     <>
       <h1>App Component</h1>
-      <div className="w-full justify-center items-center flex flex-col">
-        <ChatMessage />
-      </div>
     </>
   );
 }
