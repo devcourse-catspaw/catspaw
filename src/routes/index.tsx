96f67a01c5e0712273b2fec5f35fbd65ed219d9e
yubin121 <yubin045@naver.com>
2025-06-09 21:31:00 +0900

    🔧 Chore: 라우터 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "../pages/Home";
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from '../pages/Home';
+import GameRoomPage from '../pages/game/GameRoomPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
+    hydrateFallbackElement: <h1>Loading ...</h1>,
+  },
+  {
+    path: '/game',
+    element: <GameRoomPage />,
     hydrateFallbackElement: <h1>Loading ...</h1>,
   },
 ]);
