6720ca477e6633938333ffda148d3eecf2355417
Suji Hwang <soojiya2003@gmail.com>
2025-06-09 22:06:29 +0900

    🎨 Design: Contents.tsx 생성

---
+++ src/components/lounge/Contents.tsx
@@ -0,0 +1,15 @@
+export default function Contents() {
+  return (
+    <>
+      {/* title */}
+      <div></div>
+      {/* contents */}
+      <div></div>
+      {/* 좋아요 클릭+좋아요 수 / 댓글 수  */}
+      <div>
+        <span>24</span>
+        <span>4개의 댓글</span>
+      </div>
+    </>
+  );
+}
