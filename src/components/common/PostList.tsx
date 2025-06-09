77fa75c8a021f13afea76d505eea9f0f5fd29c00
llyybbb <yb9766@naver.com>
2025-06-09 12:17:30 +0900

    🎨 Design: 게시글 제목에 cursor pointer 와 hover: underline 추가

--- src/components/common/PostList.tsx
+++ src/components/common/PostList.tsx
@@ -9,7 +9,7 @@ export default function PostList({ postTitle, comments, date }: PostListProps) {
     <>
       <div className="w-[688px] h-[55px] px-[20px] flex justify-between items-center">
         <div className="flex justify-between items-center gap-[12px]">
-          <span className="font-semibold text-[15px] truncate max-w-[300px] overflow-hidden whitespace-nowrap">
+          <span className="font-semibold text-[15px] truncate max-w-[300px] cursor-pointer hover:underline">
             {postTitle}
           </span>
           <span className="text-[12px]">[{comments}]</span>
