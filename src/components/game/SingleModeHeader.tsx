dd412377038818b580155c8d38274c6e53d9f6d5
Jungyu Park <parkjungyu09@gmail.com>
2025-06-10 01:13:16 +0900

    ✨ Feature: SingleModeHeader 생성

---
+++ src/components/game/SingleModeHeader.tsx
@@ -0,0 +1,7 @@
+export default function SingleModeHeader () {
+  return (
+    <>
+      <h1>SingleModeHeader</h1>
+    </>
+  );
+}
---
+++ src/pages/SingleModePage.tsx
@@ -0,0 +1,7 @@
+export default function SingleModePage () {
+  return (
+    <>
+      <h1>SingleModePage</h1>
+    </>
+  );
+}
