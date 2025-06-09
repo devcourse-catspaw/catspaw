9161da0e48f115355f17b224940ad413e8f77604
Jungyu Park <parkjungyu09@gmail.com>
2025-06-09 21:44:50 +0900

    🎨 Design: 컨텐츠 위치 조정

--- src/pages/game/GameModeSelect.tsx
+++ src/pages/game/GameModeSelect.tsx
@@ -2,16 +2,22 @@ import NavWithExit from "../../components/common/NavWithExit";
 import GameModeSelectCard from "../../components/game/game_mode/GameModeSelectCard";
 import pawPencil from "../../assets/images/paw_pencil_big.svg";
 import doodle from "../../assets/images/doodle_loading.svg";
+import { useNavigate } from "react-router";
 
 export default function GameModeSelect() {
+  const navigate = useNavigate();
   return (
-    <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px]">
+    <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px] relative">
       <NavWithExit />
-      <div className="flex flex-col justify-center items-center min-h-[calc(100vh-94px)] gap-10">
+      <div className="flex flex-col justify-center items-center gap-10 absolute inset-0">
         <p className="font-extrabold text-[39px]">게임 모드를 골라보세요!</p>
         <div className="flex gap-[81px]">
-          <GameModeSelectCard mode="single" />
-          <GameModeSelectCard mode="multi" />
+          <div onClick={() => navigate("/game/single")}>
+            <GameModeSelectCard mode="single" />
+          </div>
+          <div onClick={() => navigate("/game/multi")}>
+            <GameModeSelectCard mode="multi" />
+          </div>
         </div>
       </div>
       <img
