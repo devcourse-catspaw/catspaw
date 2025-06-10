4cba85c806e30ad518b39687b11a2a6a43b3250d
Jungyu Park <parkjungyu09@gmail.com>
2025-06-11 01:32:54 +0900

    ✨ Feature: SingleModePage 컴포넌트 통합

--- src/pages/SingleModePage.tsx
+++ src/pages/SingleModePage.tsx
@@ -1,13 +1,31 @@
+import { useEffect } from "react";
 import DrawingCanvas from "../components/game/DrawingCanvas";
+import DrawingPropmt from "../components/game/DrawingPropmt";
+import GameTimer from "../components/game/GameTimer";
 import SingleModeHeader from "../components/game/SingleModeHeader";
+import { useDrawingStore } from "../stores/drawingStore";
 
 export default function SingleModePage() {
+  const { currentTopic, getRandomTopic } = useDrawingStore();
+
+  useEffect(() => {
+    getRandomTopic();
+  }, []);
+
   return (
     <>
       <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px] relative">
         <SingleModeHeader />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <DrawingCanvas />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center">
+          <div className="translate-x-7 -translate-y-2">
+            <DrawingPropmt topic={currentTopic} />
+          </div>
+          <div className="flex">
+            <DrawingCanvas />
+            <div className="translate-x-5 translate-y-4">
+              <GameTimer />
+            </div>
+          </div>
         </div>
       </div>
     </>
