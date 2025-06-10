7cf3c7f7727c59f54819a71ef29709ca0a35bd3b
Suji Hwang <soojiya2003@gmail.com>
2025-06-10 23:58:02 +0900

    🎨 Design: Comments.tsx 생성

--- src/components/lounge/Comments.tsx
+++ src/components/lounge/Comments.tsx
@@ -1,10 +1,49 @@
+import CommentCard from "./CommentCard";
+import kisu from "../../assets/images/kisu_.svg";
+import BaseInput from "../common/BaseInput";
+import Button from "../common/Button";
+
 export default function Comments() {
   return (
     <>
-      {/* 댓글 */}
-      <div></div>
-      {/* 댓글 작성 Input*/}
-      <div></div>
+      {/* 댓글 렌더링 */}
+      <div className=" flex flex-col justify-between gap-10">
+        <CommentCard
+          userName="ㅎㅎ"
+          avatar={kisu}
+          date="5분전"
+          comment="ㅍ...파이팅....."
+        />
+        <CommentCard
+          userName="hihihihi"
+          avatar={kisu}
+          date="5분전"
+          comment="ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"
+        />
+        <CommentCard
+          userName="qkrwnsrb"
+          avatar={kisu}
+          date="5분전"
+          comment="ㅉ"
+        />
+        <CommentCard
+          userName="영서쓰"
+          avatar={kisu}
+          date="5분전"
+          comment="힘내요"
+        />
+
+        {/* 댓글 입력 */}
+        <div className="w-[840px] flex justify-between">
+          <BaseInput
+            className="w-[720px] h-[40px] text-lg font-medium "
+            placeholder="댓글을 입력해주세요."
+          />
+          <Button className="w-[100px] h-[38px] text-[13px] font-bold px-[25px] py-[12px] leading-0">
+            댓글달기
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
