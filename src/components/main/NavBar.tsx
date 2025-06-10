985152b8d929d8e29876eff19496d22e7b6b4df4
llyybbb <yb9766@naver.com>
2025-06-10 10:58:36 +0900

    🎨 Design: main화면 navbar 생성

--- src/components/main/NavBar.tsx
+++ src/components/main/NavBar.tsx
@@ -1,7 +1,22 @@
+import logo_catpaw from '../../assets/images/logo_catpaw.svg'
+import logo_typo from '../../assets/images/logo_typo.svg'
+import Button from '../common/Button'
+
 export default function NavBar() {
   return (
     <>
-      <h1>NavBar Component</h1>
+      <div className="w-full px-[80px] py-[14px] flex justify-between items-center">
+        <div className="flex items-center gap-[10px]">
+          <img src={logo_catpaw} alt="cat's paw logo" className="size-[60px]" />
+          <img src={logo_typo} alt="logo text" className="h-[56px]" />
+        </div>
+        <div className="flex items-center gap-[12px]">
+          <Button className="w-[140px] h-[38px] text-[14px]"> Lounge </Button>
+          <Button className="w-[140px] h-[38px] text-[14px] px-[27px]">
+            Join to Draw
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
