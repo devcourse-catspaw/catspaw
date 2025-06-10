26cfa175d3982ecfe3eebfb4fda86d2351be615f
yubin121 <yubin045@naver.com>
2025-06-10 23:49:28 +0900

    🎨 Design: props 추가 후 수정

--- src/components/common/NavWithExit.tsx
+++ src/components/common/NavWithExit.tsx
@@ -1,19 +1,27 @@
-import logo from "../../assets/images/logo_catpaw.svg";
-import exit from "../../assets/images/icon_exit.svg";
-import { useNavigate } from "react-router";
+import logo from '../../assets/images/logo_catpaw.svg';
+import exit from '../../assets/images/icon_exit.svg';
+import { useNavigate } from 'react-router';
 
-export default function NavWithExit() {
+export default function NavWithExit({ title }: { title?: string }) {
   const navigate = useNavigate();
   return (
     <>
       <nav className="w-full flex justify-between items-center">
+        <div className="flex gap-5 items-center">
+          <img
+            src={logo}
+            alt="Cat's Paw 로고"
+            className="w-15 cursor-pointer"
+            onClick={() => navigate('/')}
+          />
+          {title && <span className="font-semibold text-lg">{title}</span>}
+        </div>
         <img
-          src={logo}
-          alt="Cat's Paw 로고"
-          className="w-15"
-          onClick={() => navigate("/")}
+          src={exit}
+          alt="나가기 버튼"
+          className="cursor-pointer"
+          onClick={() => navigate(-1)}
         />
-        <img src={exit} alt="나가기 버튼" onClick={() => navigate(-1)} />
       </nav>
     </>
   );
