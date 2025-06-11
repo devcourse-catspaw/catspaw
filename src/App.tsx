e0e6225de9b54e63d31bb67c6e9a21ffec16f99e
llyybbb <yb9766@naver.com>
2025-06-11 14:55:16 +0900

    ♻️ Refactor: useAuthInit.ts 랜더링

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import useAuthInit from './utils/useAuthInit'
+export default function App() {
+  useAuthInit()
+  return null
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import Router from "./routes/index.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+import Router from './routes/index.tsx'
+import { Toaster } from 'react-hot-toast'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
+    <App />
     <Router />
   </StrictMode>
-);
+)
