9cc9ed8327096646bb7c146762339e607ed67f59
Suji Hwang <soojiya2003@gmail.com>
2025-06-12 00:38:45 +0900

    🎨 Design: TextBox.tsx 생성

--- src/components/common/lounge/TextBox.tsx
+++ src/components/common/lounge/TextBox.tsx
@@ -1,7 +1,44 @@
-export default function TextBox() {
+import { useId, type TextareaHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+const LabeledInputStyle =
+  "border-2 border-[color:var(--black)] px-4 py-3 text-lg rounded-[6px] w-full font-medium";
+
+type LabeledInputProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  title?: string;
+  className?: string;
+  invalidMessage?: string;
+  isInvalid?: boolean;
+};
+
+export default function TextBox({
+  className,
+  invalidMessage = "한 글자 이상 입력해주세요.",
+  isInvalid = false,
+  placeholder = "이름 입력",
+  id,
+  ...props
+}: LabeledInputProps) {
+  const generatedId = useId();
+  const inputId = id ?? generatedId;
+
   return (
-    <>
-      <h1>TextBox Component</h1>
-    </>
+    <div className="flex flex-col gap-[8px]">
+      <label htmlFor={inputId} className="text-base font-semibold">
+        게시물 내용 *
+      </label>
+      <div className="flex flex-col gap-[4px] relative">
+        <textarea
+          id={inputId}
+          className={twMerge(LabeledInputStyle, className)}
+          placeholder={placeholder}
+          {...props}></textarea>
+        {isInvalid && (
+          <p className="text-[color:var(--red)] text-xs font-regular">
+            *{invalidMessage}
+          </p>
+        )}
+      </div>
+    </div>
   );
 }
