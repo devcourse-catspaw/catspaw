67dc0a43f676d11b4905843b5b6e744875152038
yubin121 <yubin045@naver.com>
2025-06-11 18:10:12 +0900

    🎨 Design: 멀티모드 결과 화면 인덱스 컴포넌트 구현

--- src/components/game/ResultPlayerIndex.tsx
+++ src/components/game/ResultPlayerIndex.tsx
@@ -1,38 +1,49 @@
 import { twMerge } from 'tailwind-merge';
 
 export default function ResultPlayerIndex({
-  avatar,
+  avatar: Avatar,
   name,
   isActive,
+  onClick,
 }: {
-  avatar: string;
+  avatar: React.FC<React.SVGProps<SVGSVGElement>>;
   name: string;
   isActive: boolean;
+  onClick: () => void;
 }) {
   return (
     <>
       <div
         className={twMerge(
-          'flex flex-col items-center p-[10px] gap-1 rounded-[5px] border-[3px] border-r-0 border-[var(--black)]',
-          isActive && 'text-[var(--red)]'
+          'flex flex-col items-center w-[50px] py-1 gap-1 rounded-[5px] border-[3px] border-r-0 border-[var(--black)] cursor-pointer',
+          isActive && 'text-[var(--red)] w-[60px] p-[10px]'
         )}
+        onClick={onClick}
       >
         <div>
-          <img
+          {/* <img
             src={avatar}
             alt="캐릭터"
             className={twMerge(
               isActive ? 'w-[40px] h-[40px]' : 'w-[30px] h-[30px]'
             )}
+          /> */}
+          <Avatar
+            className={twMerge(
+              'w-[40px] h-[40px]',
+              isActive && 'text-[var(--red)]'
+            )}
           />
         </div>
-        <div
-          className={twMerge(
-            'text-[18px] font-bold tracking-widest [writing-mode:vertical-rl] [text-orientation:sideways] rotate-180'
-          )}
-        >
-          {name}
-        </div>
+        {isActive && (
+          <div
+            className={twMerge(
+              'text-[18px] font-bold tracking-widest [writing-mode:vertical-rl] [text-orientation:sideways] rotate-180'
+            )}
+          >
+            {name}
+          </div>
+        )}
       </div>
     </>
   );
