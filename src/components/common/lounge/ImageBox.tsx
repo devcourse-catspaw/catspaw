a2c756a01b910bce8452e6a48556fd99e6a03c75
Suji Hwang <soojiya2003@gmail.com>
2025-06-12 17:09:22 +0900

    🎨 Design: ImageBox.tsx 생성

--- src/components/common/lounge/ImageBox.tsx
+++ src/components/common/lounge/ImageBox.tsx
@@ -1,6 +1,11 @@
 import deleteIcon from "../../../assets/images/icon_cancel.svg";
 
-export default function ImageBox({ image }: { image: string }) {
+type Props = {
+  image: string;
+  onDelete: () => void;
+};
+
+export default function ImageBox({ image, onDelete }: Props) {
   return (
     <div className="relative w-[142px] h-[140px] border-[1px] border-[var(--black)] flex-shrink-0">
       <img
@@ -13,6 +18,7 @@ export default function ImageBox({ image }: { image: string }) {
         src={deleteIcon}
         alt="삭제버튼"
         className="absolute top-1 right-1 w-5 h-5 cursor-pointer"
+        onClick={onDelete}
       />
     </div>
   );
