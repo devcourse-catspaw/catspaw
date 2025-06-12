38535b08cd2a446e60ba993b18a113190db4125e
yubin121 <yubin045@naver.com>
2025-06-12 11:54:30 +0900

    ✨ Feat: 결과 캡처 기능 구현 및 스크롤 애니메이션 추가

--- src/pages/game/MultiModeResult.tsx
+++ src/pages/game/MultiModeResult.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import send from '../../assets/images/icon_send.svg';
 import sketchBook from '../../assets/images/sketchbook_big.svg';
+import loading from '../../assets/images/doodle_loading.svg';
+import pawPencil from '../../assets/images/paw_pencil.svg';
 import Kisu from '../../assets/images/kisu_.svg?react';
 import NavWithExit from '../../components/common/NavWithExit';
-// import DrawingCanvas from '../../components/game/DrawingCanvas';
 import { useNavigate } from 'react-router';
 import ChatMessage from '../../components/common/ChatMessage';
 import BaseInput from '../../components/common/BaseInput';
@@ -12,11 +13,15 @@ import ResultChat from '../../components/game/ResultChat';
 import logoTypo from '../../assets/images/logo_typo.svg';
 import ResultPlayerIndex from '../../components/game/ResultPlayerIndex';
 import ResultShareModal from '../../components/game/ResultShareModal';
+import html2canvas from 'html2canvas';
+import ScrollItem from '../../components/common/ScrollItem';
 
 export default function MultiModeResult() {
   const navigate = useNavigate();
 
   const [isActive, setIsActive] = useState([true, false, false, false]);
+  const [imageUrl, setImageUrl] = useState('');
+  const [isCapturing, setIsCapturing] = useState(false);
   const [isResultShareModalOpen, setIsResultShareModalOpen] = useState(false);
 
   const [msg, setMsg] = useState('');
@@ -26,6 +31,8 @@ export default function MultiModeResult() {
 
   const bottomRef = useRef<HTMLDivElement | null>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const divModifyRef = useRef<HTMLDivElement | null>(null);
 
   const clickPlayerIndexHandler = (index: number) => {
     const newArr = [false, false, false, false];
@@ -63,8 +70,41 @@ export default function MultiModeResult() {
     navigate('/');
   };
 
-  const clickShareHandler = () => {
-    setIsResultShareModalOpen(true);
+  const clickShareHandler = async () => {
+    const div = divRef.current;
+    const divModify = divModifyRef.current;
+    if (!div || !divModify) return;
+
+    div.classList.remove('overflow-hidden');
+    div.classList.add('h-full');
+    divModify.classList.remove('overflow-y-auto');
+    divModify.classList.add('h-full');
+
+    // const originalHeight = divModify.style.height;
+    // divModify.style.height = divModify.scrollHeight + 'px';
+
+    try {
+      setIsCapturing(true);
+      const canvas = await html2canvas(div, {
+        useCORS: true,
+        allowTaint: true,
+        scale: 2,
+      });
+
+      const dataUrl = canvas.toDataURL('image/png');
+      setImageUrl(dataUrl);
+      setIsResultShareModalOpen(true);
+    } catch (e) {
+      console.error('캡처 실패:', e);
+    } finally {
+      div.classList.add('overflow-hidden');
+      div.classList.remove('h-full');
+      divModify.classList.add('overflow-y-auto');
+      divModify.classList.remove('h-full');
+      setIsCapturing(false);
+
+      // divModify.style.height = originalHeight;
+    }
   };
 
   const closeResultShareModalHandler = () => {
@@ -85,155 +125,196 @@ export default function MultiModeResult() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px] relative">
       <NavWithExit title="같이 할 사람~" />
-      <div className="flex flex-col gap-[55px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        {/* absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 */}
-        <div className="flex gap-7 ">
-          <div className="flex flex-col gap-5 items-end">
-            <div className="w-[629px] h-[62px] flex justify-center items-center text-[18px] font-semibold bg-[var(--white)] rounded-[6px] border-2 border-[var(--black)]">
-              결과 발표
-            </div>
-            {/* <DrawingCanvas onSubmit={sendDrawingHandler} /> */}
-            <div className="flex relative">
-              {/* flex flex-col justify-end items-end */}
-              <div className="absolute top-0 -left-[58px] flex flex-col items-end w-[64px] mt-5">
-                <ResultPlayerIndex
-                  avatar={Kisu}
-                  name="트랄랄라트랄랄라"
-                  isActive={isActive[0]}
-                  onClick={() => clickPlayerIndexHandler(0)}
-                />
-                <ResultPlayerIndex
-                  avatar={Kisu}
-                  name="유빈bin123"
-                  isActive={isActive[1]}
-                  onClick={() => clickPlayerIndexHandler(1)}
-                />
-                <ResultPlayerIndex
-                  avatar={Kisu}
-                  name="수코딩"
-                  isActive={isActive[2]}
-                  onClick={() => clickPlayerIndexHandler(2)}
-                />
-                <ResultPlayerIndex
-                  avatar={Kisu}
-                  name="Hello"
-                  isActive={isActive[3]}
-                  onClick={() => clickPlayerIndexHandler(3)}
-                />
+      {isCapturing ? (
+        <div className="relative w-full min-h-screen">
+          <img
+            src={loading}
+            alt="라인"
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            src={pawPencil}
+            alt="연필"
+            className="absolute top-[37%] left-[65%] -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-[55px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {/* absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 */}
+          <div className="flex gap-7 ">
+            <div className="flex flex-col gap-5 items-end">
+              <div className="w-[629px] h-[62px] flex justify-center items-center text-[18px] font-semibold bg-[var(--white)] rounded-[6px] border-2 border-[var(--black)]">
+                결과 발표
               </div>
-              <div className="flex relative w-[629px] h-[495px] justify-center items-center pt-9 pr-2 overflow-hidden">
-                <div className="w-[610px] h-[440px] p-5 overflow-y-auto">
-                  <ResultChat
-                    userName="트랄랄라"
-                    message="푸바오"
-                    isDrawing={false}
+              <div className="flex relative">
+                {/* flex flex-col justify-end items-end */}
+                <div className="absolute top-0 -left-[58px] flex flex-col items-end w-[64px] mt-5">
+                  <ResultPlayerIndex
+                    avatar={Kisu}
+                    name="트랄랄라트랄랄라"
+                    isActive={isActive[0]}
+                    onClick={() => clickPlayerIndexHandler(0)}
                   />
-                  <ResultChat
-                    userName="꾸꾸까까"
-                    message={logoTypo}
-                    isDrawing={true}
+                  <ResultPlayerIndex
+                    avatar={Kisu}
+                    name="유빈bin123"
+                    isActive={isActive[1]}
+                    onClick={() => clickPlayerIndexHandler(1)}
                   />
-                  <ResultChat
-                    userName="힣힣힣ㅎ"
-                    message="너구리"
-                    isDrawing={false}
+                  <ResultPlayerIndex
+                    avatar={Kisu}
+                    name="수코딩"
+                    isActive={isActive[2]}
+                    onClick={() => clickPlayerIndexHandler(2)}
                   />
-                  <ResultChat
-                    userName="Micky Park"
-                    message={logoTypo}
-                    isDrawing={true}
+                  <ResultPlayerIndex
+                    avatar={Kisu}
+                    name="Hello"
+                    isActive={isActive[3]}
+                    onClick={() => clickPlayerIndexHandler(3)}
                   />
                 </div>
-                <img
-                  src={sketchBook}
-                  alt="스케치북"
-                  className="absolute inset-0 w-full h-full -z-50"
-                  // -right-8 bottom-0
+                <div
+                  // id="scroll-container"
+                  ref={divRef}
+                  // h-full overflow-hidden
+                  className="flex relative w-[629px] h-[495px] justify-center items-center pt-9 pr-2 overflow-hidden"
+                >
+                  {/* h-full overflow-y-auto */}
+                  <div
+                    id="scroll-container"
+                    ref={divModifyRef}
+                    className="w-[610px] h-[440px] p-5 overflow-y-auto"
+                  >
+                    {/* <ScrollItem> */}
+                    <ScrollItem delay={0.5}>
+                      <ResultChat
+                        userName="트랄랄라"
+                        message="푸바오"
+                        isDrawing={false}
+                      />
+                    </ScrollItem>
+                    {/* <ScrollItem> */}
+                    <ScrollItem delay={1.0}>
+                      <ResultChat
+                        userName="꾸꾸까까"
+                        message={logoTypo}
+                        isDrawing={true}
+                      />
+                    </ScrollItem>
+                    {/* <ScrollItem> */}
+                    <ScrollItem delay={1.5}>
+                      <ResultChat
+                        userName="힣힣힣ㅎ"
+                        message="너구리"
+                        isDrawing={false}
+                      />
+                    </ScrollItem>
+                    {/* <ScrollItem> */}
+                    <ScrollItem delay={0.3}>
+                      <ResultChat
+                        userName="Micky Park"
+                        message={logoTypo}
+                        isDrawing={true}
+                      />
+                    </ScrollItem>
+                  </div>
+                  <img
+                    src={sketchBook}
+                    alt="스케치북"
+                    className="absolute inset-0 w-full h-full -z-50"
+                    // -right-8 bottom-0
+                  />
+                </div>
+              </div>
+            </div>
+            {/* h-[480px]  */}
+            <div className="flex flex-col w-[287px] bg-[var(--white)] rounded-[6px] border-2 border-[var(--black)] shadow-[0_4px_4px_rgba(0,0,0,0.25)] overflow-hidden">
+              <div className="h-15 font-bold text-[18px] flex justify-center items-center">
+                채팅
+              </div>
+              <div className="h-[440px] flex flex-col gap-2 px-4 border-y-2 border-[var(--black)] overflow-y-auto">
+                <div className="py-2 space-y-2">
+                  <ChatMessage
+                    userName="유코딩"
+                    message="안녕하세욥!"
+                    isMine={false}
+                    size="small"
+                  />
+                  <ChatMessage
+                    userName="유코딩"
+                    message="그림 잘 그리세요? 사실 전 그림 못 그리는 사람과는 하고 싶지 않거든요"
+                    isMine={false}
+                    size="small"
+                  />
+                  <ChatMessage
+                    userName="Yubin"
+                    message="네"
+                    isMine={true}
+                    size="small"
+                  />
+                  <ChatMessage
+                    userName="유코딩"
+                    message="ㅎㅎ"
+                    isMine={false}
+                    size="small"
+                  />
+                  <ChatMessage
+                    userName="Yubin"
+                    message="네네네네네네네네ㅔㄴ네네네ㅔㄴ네ㅔㅔ네네ㅔ네네네네네ㅔ네"
+                    isMine={true}
+                    size="small"
+                  />
+                  <ChatMessage
+                    userName="Yubin"
+                    message="네네네ㅔㄴ네네ㅔㅔ네ㅔ네네ㅔ네ㅔ"
+                    isMine={true}
+                    size="small"
+                  />
+                </div>
+                <div ref={bottomRef}></div>
+              </div>
+              <div className="flex justify-between gap-[10px] px-4 py-4">
+                <BaseInput
+                  ref={inputRef}
+                  value={msg}
+                  onChange={(e) => setMsg(e.target.value)}
+                  onKeyDown={keyDownHandler}
+                  placeholder="메시지 입력"
+                  className="text-[14px] h-10"
                 />
+                <Button
+                  onClick={sendMessageHandler}
+                  className="w-[46px] h-[35px] px-2 py-0"
+                >
+                  <img
+                    src={send}
+                    alt="전송"
+                    className="w-[20px] h-[20px]"
+                  ></img>
+                </Button>
               </div>
             </div>
           </div>
-          {/* h-[480px]  */}
-          <div className="flex flex-col w-[287px] bg-[var(--white)] rounded-[6px] border-2 border-[var(--black)] shadow-[0_4px_4px_rgba(0,0,0,0.25)] overflow-hidden">
-            <div className="h-15 font-bold text-[18px] flex justify-center items-center">
-              채팅
-            </div>
-            <div className="h-[440px] flex flex-col gap-2 px-4 border-y-2 border-[var(--black)] overflow-y-auto">
-              <div className="py-2 space-y-2">
-                <ChatMessage
-                  userName="유코딩"
-                  message="안녕하세욥!"
-                  isMine={false}
-                  size="small"
-                />
-                <ChatMessage
-                  userName="유코딩"
-                  message="그림 잘 그리세요? 사실 전 그림 못 그리는 사람과는 하고 싶지 않거든요"
-                  isMine={false}
-                  size="small"
-                />
-                <ChatMessage
-                  userName="Yubin"
-                  message="네"
-                  isMine={true}
-                  size="small"
-                />
-                <ChatMessage
-                  userName="유코딩"
-                  message="ㅎㅎ"
-                  isMine={false}
-                  size="small"
-                />
-                <ChatMessage
-                  userName="Yubin"
-                  message="네네네네네네네네ㅔㄴ네네네ㅔㄴ네ㅔㅔ네네ㅔ네네네네네ㅔ네"
-                  isMine={true}
-                  size="small"
-                />
-                <ChatMessage
-                  userName="Yubin"
-                  message="네네네ㅔㄴ네네ㅔㅔ네ㅔ네네ㅔ네ㅔ"
-                  isMine={true}
-                  size="small"
-                />
-              </div>
-              <div ref={bottomRef}></div>
-            </div>
-            <div className="flex justify-between gap-[10px] px-4 py-4">
-              <BaseInput
-                ref={inputRef}
-                value={msg}
-                onChange={(e) => setMsg(e.target.value)}
-                onKeyDown={keyDownHandler}
-                placeholder="메시지 입력"
-                className="text-[14px] h-10"
-              />
-              <Button
-                onClick={sendMessageHandler}
-                className="w-[46px] h-[35px] px-2 py-0"
-              >
-                <img src={send} alt="전송" className="w-[20px] h-[20px]"></img>
-              </Button>
-            </div>
+          <div className="flex justify-center items-center gap-[39px]">
+            <Button
+              onClick={clickShareHandler}
+              className="w-[134px] h-[44px] px-0 py-0 text-[18px] z-10"
+            >
+              결과 공유
+            </Button>
+            <Button
+              onClick={clickExitHandler}
+              className="w-[134px] h-[44px] px-0 py-0 text-[18px]"
+            >
+              방 나가기
+            </Button>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-[39px]">
-          <Button
-            onClick={clickShareHandler}
-            className="w-[134px] h-[44px] px-0 py-0 text-[18px]"
-          >
-            결과 공유
-          </Button>
-          <Button
-            onClick={clickExitHandler}
-            className="w-[134px] h-[44px] px-0 py-0 text-[18px]"
-          >
-            방 나가기
-          </Button>
-        </div>
-      </div>
+      )}
       {isResultShareModalOpen && (
         <ResultShareModal
+          imageUrl={imageUrl}
           closeResultShareModalHandler={closeResultShareModalHandler}
         />
       )}
