60fcc127c61096f46a44aaf90e05865a906f1b2c
Jungyu Park <parkjungyu09@gmail.com>
2025-06-12 16:57:01 +0900

    ✨ Feature: 게임 모드 선택 화면 초기 랜더링 시 storage 및 전역 상태 초기화

--- src/pages/game/GameModeSelect.tsx
+++ src/pages/game/GameModeSelect.tsx
@@ -4,10 +4,15 @@ import GameModeSelectCard from "../../components/game/game_mode/GameModeSelectCa
 import NavWithExit from "../../components/common/NavWithExit";
 import pawPencil from "../../assets/images/paw_pencil_big.svg";
 import doodle from "../../assets/images/doodle_loading.svg";
+import supabase from "../../utils/supabase";
+import { useAuthStore } from "../../stores/authStore";
+import { useDrawingStore } from "../../stores/drawingStore";
 
 export default function GameModeSelect() {
   const navigate = useNavigate();
   const [count, setCount] = useState<number | null>(null);
+  const user = useAuthStore((state) => state.user);
+  const { resetTopicList, getRandomTopic } = useDrawingStore();
 
   useEffect(() => {
     if (count === null) return;
@@ -21,6 +26,33 @@ export default function GameModeSelect() {
     return () => clearTimeout(timer);
   }, [count, navigate]);
 
+  useEffect(() => {
+    const clearData = async () => {
+      const { data, error } = await supabase.storage
+        .from("singlemode-images")
+        .list(`private/${user?.id}`);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const fileNames = data.map((file) => `private/${user?.id}/${file.name}`);
+
+      if (fileNames.length > 0) {
+        const { error } = await supabase.storage
+          .from("singlemode-images")
+          .remove(fileNames);
+        if (error) {
+          console.error(error);
+        }
+      }
+      resetTopicList();
+      getRandomTopic();
+    };
+    clearData();
+  });
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center px-20 pt-[14px] relative">
       <NavWithExit />
@@ -31,10 +63,7 @@ export default function GameModeSelect() {
               게임 모드를 골라보세요!
             </p>
             <div className="flex gap-[81px]">
-              <div
-                onClick={() => setCount(3)} 
-                className="z-50 cursor-pointer"
-              >
+              <div onClick={() => setCount(3)} className="z-50 cursor-pointer">
                 <GameModeSelectCard mode="single" />
               </div>
               <div onClick={() => navigate("/game/multi")} className="z-50">
