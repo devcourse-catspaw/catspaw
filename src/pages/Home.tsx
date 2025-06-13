f42a34a91426327f0ef55d22d3eeb9bb4390f20c
llyybbb <yb9766@naver.com>
2025-06-13 11:36:27 +0900

    ✨ Feat: ScrollSmoother 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,11 @@ import MainBGDoodle3 from '../components/main/animation/MainBGDoodle3'
 import MainBGDoodle4 from '../components/main/animation/MainBGDoodle4'
 import MainLank from '../components/main/animation/MainLank'
 import AboutUs from '../components/main/animation/AboutUs'
+import gsap from 'gsap'
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
 export default function Home() {
   useEffect(() => {
     supabase.auth.getUser().then(({ data }) => {
@@ -18,22 +23,32 @@ export default function Home() {
         toast.error('유저 정보를 불러오기 실패')
       }
     })
+
+    ScrollSmoother.create({
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+      smooth: 1.5,
+    })
   }, [])
 
   return (
     <>
-      <NavBar />
-      <div className="flex flex-col justify-center w-full relative">
-        <img src={main_catdoodle} className="w-full" />
-        <MainBGDoodle1 />
-        <MainLank />
-        <MainBGDoodle2 />
-        <div className="w-full aspect-[1156/613] px-[80px] my-[100px]">
-          <div className="w-full h-[80%] bg-[var(--grey-200)] rounded-[24px]"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <NavBar />
+          <div className="flex flex-col justify-center w-full relative">
+            <img src={main_catdoodle} className="w-full" />
+            <MainBGDoodle1 />
+            <MainLank />
+            <MainBGDoodle2 />
+            <div className="w-full aspect-[1156/613] px-[80px] my-[100px]">
+              <div className="w-full h-[80%] bg-[var(--grey-200)] rounded-[24px]"></div>
+            </div>
+            <MainBGDoodle3 />
+            <AboutUs />
+            <MainBGDoodle4 />
+          </div>
         </div>
-        <MainBGDoodle3 />
-        <AboutUs />
-        <MainBGDoodle4 />
       </div>
     </>
   )
