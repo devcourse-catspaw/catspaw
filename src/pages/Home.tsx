8d2b67f444520fe76396ae295d43a5d7d9e246e7
llyybbb <yb9766@naver.com>
2025-06-15 01:03:10 +0900

    🎨 Design: HotPost 컴포넌트 swiper 적용, maindoodle 에 다크모드 버튼, 말풍선 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,19 @@ import MainBGDoodle3 from '../components/main/animation/MainBGDoodle3'
 import MainBGDoodle4 from '../components/main/animation/MainBGDoodle4'
 import MainLank from '../components/main/animation/MainLank'
 import AboutUs from '../components/main/animation/AboutUs'
+import main_speech_bubble from '../assets/images/main_speech_bubble.svg'
+import main_cat_lightmode from '../assets/images/main_cat_lightmode.PNG'
 import gsap from 'gsap'
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all'
+import HotPost from '../components/main/HotPost'
+import Button from '../components/common/Button'
+import Fire from '../components/main/animation/Fire'
+import { useNavigate } from 'react-router-dom'
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 export default function Home() {
+  const navigate = useNavigate()
   useEffect(() => {
     supabase.auth.getUser().then(({ data }) => {
       if (data.user) {
@@ -36,13 +43,44 @@ export default function Home() {
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <NavBar />
-          <div className="flex flex-col justify-center w-full relative">
-            <img src={main_catdoodle} className="w-full" />
+          <div className="flex flex-col justify-center w-full ">
+            <div
+              className="relative w-full aspect-[16/9] bg-cover bg-no-repeat "
+              style={{ backgroundImage: `url(${main_catdoodle})` }}
+            >
+              <img
+                className="absolute right-[3vw] top-[10vh] w-[35vw] aspect-[483/384]"
+                src={main_speech_bubble}
+                alt="speech_bubble"
+              />
+              <img
+                className="absolute bottom-[10vw] left-1/2 -translate-x-[81%] size-[5vw] cursor-pointer"
+                src={main_cat_lightmode}
+                alt="main_cat_lightmode"
+              />
+              <Button
+                onClick={() => navigate('/game')}
+                className="w-[305px] h-[46px] absolute bottom-[20px] left-1/2 -translate-x-1/2 bg-[var(--red)] cursor-pointer"
+              >
+                Start To Draw
+              </Button>
+            </div>
             <MainBGDoodle1 />
             <MainLank />
             <MainBGDoodle2 />
-            <div className="w-full aspect-[1156/613] px-[80px] my-[100px]">
-              <div className="w-full h-[80%] bg-[var(--grey-200)] rounded-[24px]"></div>
+            <div className=" w-full aspect-[1156/613] px-[80px] my-[100px]">
+              <div className=" flex flex-col gap-[10vh] w-full h-[80%] px-[40px] py-[20px] bg-[var(--grey-200)] rounded-[24px]">
+                <div className="flex justify-center items-end gap-[54px] -translate-y-[50px]">
+                  <Fire />
+                  <span className="text-[var(--white)] text-[5vw] font-bold pb-[27px]">
+                    What's Hot
+                  </span>
+                  <Fire />
+                </div>
+                <div className="relative ">
+                  <HotPost />
+                </div>
+              </div>
             </div>
             <MainBGDoodle3 />
             <AboutUs />
