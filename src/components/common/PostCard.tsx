7d5ec95a13aa5156fcd7160f7313da91cef6fb46
Suji Hwang <soojiya2003@gmail.com>
2025-06-16 14:38:19 +0900

    🎨 Design: 포스트 날짜 UI 수정

--- src/components/common/PostCard.tsx
+++ src/components/common/PostCard.tsx
@@ -26,7 +26,7 @@ type PostCardProps = {
 const cardLayout =
   "w-[240px] h-[324px] border-[3px] border-[var(--black)] shadow-[0px_7px_0px_var(--black)] bg-[var(--white)] rounded-[11px] flex";
 const titleStyle =
-  "text-[var(--black)]  text-base font-bold block w-100% overflow-hidden overflow-ellipsis whitespace-nowrap";
+  "min-w-[150px] text-[var(--black)] text-base font-bold block w-100% overflow-hidden overflow-ellipsis whitespace-nowrap";
 const contentStyle =
   "text-[var(--black)] text-sm font-medium block w-100% overflow-hidden overflow-ellipsis whitespace-nowrap";
 const likeCountStyle =
--- src/components/lounge/TimeAgo.tsx
+++ src/components/lounge/TimeAgo.tsx
@@ -1,6 +1,8 @@
 interface TimeAgoProps {
   timestamp: string | number | Date;
 }
+const dateStyle =
+  " text-end min-w-[46px] text-[var(--grey-100)] text-xs  font-medium"; // 기본 폰트 클래스
 
 // 사용법: <TimeAgo timestamp={post.created_at /> props로 api를 통해 받은 created_at을 넘겨준다
 // api 예시 created_at: "2021-10-15T20:48:19.816Z",
@@ -10,8 +12,6 @@ export default function TimeAgo({
   const now = new Date();
   const time = new Date(timestamp);
   const diffInSeconds = Math.floor((now.getTime() - time.getTime()) / 1000);
-
-  const dateStyle = "text-[var(--grey-100)] text-xs  font-medium"; // 기본 폰트 클래스
 
   if (diffInSeconds < 60) {
     return <span className={dateStyle}>{`${diffInSeconds}초 전`}</span>;
@@ -32,10 +32,12 @@ export default function TimeAgo({
     return <span className={dateStyle}>{`${diffInDays}일 전`}</span>;
   }
 
-  // 7일 이상일 때는 년월일 출력
+  // 1일 이상일 때는 년월일 출력
   const year = time.getFullYear();
+  const yearStr = String(year);
+  const shortYear = yearStr.slice(-2);
   const month = time.getMonth() + 1;
   const day = time.getDate();
 
-  return <span className={dateStyle}>{`${year}년 ${month}월 ${day}일`}</span>;
+  return <span className={dateStyle}>{`${shortYear}. ${month}. ${day}`}</span>;
 }
