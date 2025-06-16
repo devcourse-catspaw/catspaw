c657f9bc77aedffcc34665b6d971df9a3106bff7
Suji Hwang <soojiya2003@gmail.com>
2025-06-16 09:17:22 +0900

    ✨ Feat: 댓글 추가 기능 구현

--- src/components/lounge/Comments.tsx
+++ src/components/lounge/Comments.tsx
@@ -5,59 +5,77 @@ import Button from "../common/Button";
 import type { fetchPostDetail } from "../../routes/loader/post.loader";
 import { useState } from "react";
 import supabase from "../../utils/supabase";
+import { useAuthStore } from "../../stores/authStore";
 
 type PostDetail = Awaited<ReturnType<typeof fetchPostDetail>>;
+//TODO 오류 확인 필요
 type CommentRow = PostDetail["comments"][number];
+
+// type CommentRow = {
+//   content: string;
+//   created_at: string;
+//   id: number;
+//   post_id: number;
+//   updated_at: string;
+//   user_id: string;
+//   userName: string;
+//   avatar: string;
+//   date: string;
+//   comment: string;
+// };
 type CommentsProps = {
   postId: number;
   initialComments: CommentRow[];
 };
 
 export default function Comments({ postId, initialComments }: CommentsProps) {
+  const user = useAuthStore((state) => state.user);
   const [comments, setComments] = useState<CommentRow[]>(initialComments);
   const [newComment, setNewComment] = useState("");
 
   const handleAddComment = async () => {
     if (!newComment.trim()) return;
 
-    //TODO 오류 확인 필요
+    console.log(comments);
+
     const { data, error } = await supabase
       .from("comments")
       .insert([
         {
           post_id: postId,
           content: newComment,
+          user_id: user!.id,
         },
       ])
-      .select()
-      .single();
+      .select();
+    // .single();
 
     if (error || !data) {
       console.error("댓글 추가 실패", error);
       return;
     }
 
-    // 2) 로컬 배열에 추가 (optimistic UI)
+    // 로컬 배열에 추가 (optimistic UI)
     setComments((prev) => [...prev, data]);
     setNewComment("");
+    console.log(comments);
   };
 
   return (
     <>
-      {" "}
-      q{/* 댓글 렌더링 */}
+      {/* 댓글 렌더링 */}
       <div className=" flex flex-col gap-[40px]">
         <div className="flex flex-col justify-between divide-y divide-[var(--grey-100)]">
-          {comments.map((c) => (
+          {comments.map((c, idx) => (
             <CommentCard
-              key={c.id}
+              key={`${c.users.id}-${idx}`}
               id={c.id}
               userId={c.users.id}
-              userName={c.userName}
-              avatar={c.avatar}
-              date={c.date}
-              comment={c.comment}
-              onDelete={() => (deletedId: string) =>
+              userName={c.users.nickname}
+              avatar={kisu}
+              date={c.updated_at}
+              comment={c.content}
+              onDelete={() => (deletedId: number) =>
                 setComments((prev) => prev.filter((x) => x.id !== deletedId))}
             />
           ))}
