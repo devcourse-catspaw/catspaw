4d3342f8239652034c41bec17867528f45616199
Suji Hwang <soojiya2003@gmail.com>
2025-06-17 04:24:28 +0900

    🐛 Fix: 수퍼베이스 게시물 저장 및 삭제 오류 수정

--- src/pages/lounge/EditPost.tsx
+++ src/pages/lounge/EditPost.tsx
@@ -10,6 +10,7 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 import supabase from "../../utils/supabase";
 import toast from "react-hot-toast";
 import type { fetchExactPost } from "../../routes/loader/post.loader";
+import { useAuthStore } from "../../stores/authStore";
 
 const cardLayout =
   "w-[1080px]  flex flex-col items-center overflow-visible border-[3px] border-[var(--black)] shadow-[0px_7px_0px_var(--black)] rounded-[11px] ";
@@ -18,30 +19,94 @@ type PostDetail = Awaited<ReturnType<typeof fetchExactPost>>;
 
 export default function EditPost() {
   const post = useLoaderData<PostDetail>();
-  // const user = useAuthStore((s) => s.user)!;
+  const user = useAuthStore((s) => s.user)!;
   const inputId = useId();
   const navigate = useNavigate();
 
   const [title, setTitle] = useState(post?.title);
   const [content, setContent] = useState(post?.content);
   const [imgPath, setImgPath] = useState<string[]>(post?.images ?? []);
+  const [files, setFiles] = useState<File[]>([]);
 
   const addImage = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (!files) return;
 
+    const newFiles = event.target.files ? Array.from(event.target.files) : [];
+    if (newFiles.length === 0) return;
+
+    setFiles((prev) => [...prev, ...newFiles]);
+
     const newPaths = Array.from(files).map((file) => URL.createObjectURL(file));
     setImgPath((prev) => [...prev, ...newPaths]);
   };
 
-  const deleteImage = (idx: number) => {
-    setImgPath((p) => p.filter((_, i) => i !== idx));
+  const uploadAll = async (): Promise<string[]> => {
+    const uploadPromises = files.map(async (file) => {
+      // 파일명 새로 생성
+      const ext = file.name.split(".").pop();
+      const fileName = `${Date.now()}.${ext}`;
+      const filePath = `posts/${user!.id}/${fileName}`;
+
+      // 업로드
+      const { error: uploadError } = await supabase.storage
+        .from("post-images")
+        .upload(filePath, file);
+
+      if (uploadError) console.error("에러", uploadError);
+
+      // 공개 URL 가져오기
+      const { data: urlData } = supabase.storage
+        .from("post-images")
+        .getPublicUrl(filePath);
+      return urlData.publicUrl;
+    });
+
+    // 모든 업로드가 끝날 때까지 대기 후 URL 리스트 리턴
+    return Promise.all(uploadPromises);
+  };
+
+  const deleteImage = async (idx: number) => {
+    const postImg = post.images![idx];
+
+    const updatedImgPath = imgPath.filter((_, i) => i !== idx);
+    const updatedFiles = files.filter((_, i) => i !== idx);
+
+    setImgPath(updatedImgPath);
+    setFiles(updatedFiles);
+
+    const deleteImgPath = postImg.split("/").slice(-1);
+    const { data, error } = await supabase.storage
+      .from("post-images")
+      .remove([`posts/${user.id}/${deleteImgPath}`]);
+
+    const updatedPostImg = post.images!.filter((_, i) => i !== idx);
+
+    const { error: updatedError } = await supabase
+      .from("posts")
+      .update({ images: updatedPostImg })
+      .eq("id", post!.id);
+
+    if (updatedError) {
+      console.error(updatedError);
+      toast.error("수정에 실패했습니다.");
+      return;
+    }
+
+    post.images = updatedPostImg;
+
+    if (error) console.error(error);
+    if (data) console.log(data);
   };
 
   const handleSubmit = async () => {
+    const prevImageUrls = [...post.images!];
+    const newImageUrls = await uploadAll();
+    const AllImageUrls = [...prevImageUrls, ...newImageUrls];
+
     const { error } = await supabase
       .from("posts")
-      .update({ title, content, images: imgPath })
+      .update({ title, content, images: AllImageUrls })
       .eq("id", post!.id);
 
     if (error) {
@@ -49,6 +114,7 @@ export default function EditPost() {
       toast.error("수정에 실패했습니다.");
       return;
     }
+
     setTitle(title);
     setContent(content);
     setImgPath(imgPath);
@@ -81,7 +147,7 @@ export default function EditPost() {
 
             <TextBox
               value={content}
-              onChange={(e) => setTitle(e.target.value)}
+              onChange={(e) => setContent(e.target.value)}
               placeholder="내용 입력"
               className="w-[915px] h-[200px] align-text-top"
             />
