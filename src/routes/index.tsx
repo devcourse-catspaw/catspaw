79cb6205e28487e46fc3964b0fe815583d14a135
Suji Hwang <soojiya2003@gmail.com>
2025-06-17 10:41:57 +0900

    🐛 Fix: 포스트 수정 페이지 라우트 loader 재설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,34 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import Home from '../pages/Home'
-import GameModeSelect from '../pages/game/GameModeSelect'
-import GameLayout from './layouts/GameLayout'
-import SingleModePage from '../pages/SingleModePage'
-import Login from '../pages/Login'
-import useAuthInit from './../utils/useAuthInit'
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import Home from "../pages/Home";
+import GameModeSelect from "../pages/game/GameModeSelect";
+import GameLayout from "./layouts/GameLayout";
+import SingleModePage from "../pages/SingleModePage";
+import Login from "../pages/Login";
+import Lounge from "../pages/Lounge";
+import LoungeLayout from "./layouts/LoungeLayout";
+import {
+  fetchExactPost,
+  fetchPostDetail,
+  fetchPosts,
+} from "./loader/post.loader";
+import useAuthInit from "./../utils/useAuthInit";
+import EditPost from "../pages/lounge/EditPost";
+import AddPost from "../pages/lounge/AddPost";
+import LoungeDetail from "../pages/lounge/LoungeDetail";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
     hydrateFallbackElement: <h1>Loading ...</h1>,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
     hydrateFallbackElement: <h1>Loading ...</h1>,
   },
   {
-    path: '/game',
+    path: "/game",
     element: <GameLayout />,
     children: [
       {
@@ -26,21 +36,35 @@ const router = createBrowserRouter([
         element: <Navigate to="select" replace />,
       },
       {
-        path: 'select',
+        path: "select",
         element: <GameModeSelect />,
       },
       {
-        path: 'single',
+        path: "single",
         element: <SingleModePage />,
       },
     ],
   },
-])
+  {
+    path: "/lounge",
+    element: <LoungeLayout />,
+    children: [
+      { index: true, element: <Lounge />, loader: fetchPosts },
+      { path: ":id", element: <LoungeDetail />, loader: fetchPostDetail },
+      { path: "add-post", element: <AddPost /> },
+      {
+        path: "edit-post/:postId",
+        element: <EditPost />,
+        loader: fetchExactPost,
+      },
+    ],
+  },
+]);
 export default function Router() {
-  useAuthInit()
+  useAuthInit();
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
